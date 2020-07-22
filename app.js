2633993d393c1881a189c5ad29ec0262c5659c58
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-22 14:26:28 +0200

    create first routes

--- app.js
+++ app.js
@@ -1,9 +1,61 @@
 const express = require('express')
 const app = express()
+const connection = require('./conf');
+const { json } = require('express');
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true
+}));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
+
+app.get('/projects', (req, res) => {
+  connection.query('SELECT * FROM projects', (err, results) => {
+    if (err) {
+      res.status(500).send('Error server');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/projects/:id', (req, res) => {
+  const projectsId = req.params.id;
+  connection.query('SELECT * FROM projects WHERE id = ?', [projectsId], (err, results) => {
+    if (err) {
+      res.status(500).send('Error server');
+    } else if (results[0] === undefined) {
+      res.status(404).send("Project not found")
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/languages', (req, res) => {
+  connection.query('SELECT * FROM languages', (err, results) => {
+    if (err) {
+      res.status(500).send('Error server');
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+app.get('/languages/:id', (req, res) => {
+  const languagesId = req.params.id;
+  connection.query('SELECT * FROM languages WHERE id = ?', [languagesId], (err, results) => {
+    if (err) {
+      res.status(500).send('Error server');
+    } else if (results[0] === undefined) {
+      res.status(404).send("Language not found")
+    } else {
+      res.json(results);
+    }
+  });
+});
 
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
