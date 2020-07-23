ca4f7d868fbba053027ea57c28edfe4f593ad601
Victorafx <victor.auxietrefontes@laposte.net>
2020-07-23 09:34:09 +0200

    add cors to project

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const connection = require('./conf');
+
+const cors = require('cors');
+app.use(cors());
+
 const { json } = require('express');
 app.use(express.json());
 app.use(express.urlencoded({
